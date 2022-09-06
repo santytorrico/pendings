af2f4a6ce28c0ae3d1e1cddd2fd78f9f029c8813
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-06 10:01:04 -0400

    Addded function to input an activity and show it

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+
+const desc = document.querySelector("#activity");
+const form = document.querySelector("#todo-form");
+const div = document.querySelector("#todo-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const description= desc.value;
+  div.innerHTML = "<p>"+ `To do : ${description}` +"<p/>"
 });
