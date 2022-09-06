c95cfd30267e80d7043f511f9f96454acdd5b98c
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-06 10:08:51 -0400

    final

--- src/presenter.js
+++ src/presenter.js
@@ -7,5 +7,28 @@ const div = document.querySelector("#todo-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const description= desc.value;
+  required(description);
+  len(description);
   div.innerHTML = "<p>"+ `To do : ${description}` +"<p/>"
 });
+
+function required(inputtx) 
+   {
+     if (inputtx.length == 0)
+      { 
+         alert("You didn't put anything");  	
+         return false; 
+      }  	
+      return true; 
+    } 
+
+    function len(inputtx) 
+    {
+      if (inputtx.length <= 50)
+       { 
+          alert("You didn't put anything");  	
+          return false; 
+       }  	
+       return true; 
+     } 
+  
